2e6ac6084a6193b3ff4674efab8fae8feb08613e
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-05-22 20:55:36 -0300

    [orgs/groups][xs] - remove bulk edit

--- src/pages/dashboard/groups/index.tsx
+++ src/pages/dashboard/groups/index.tsx
@@ -84,12 +84,6 @@ const GroupsDashboard: NextPage = () => {
                       type="button"
                       className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
                     >
-                      Bulk edit
-                    </button>
-                    <button
-                      type="button"
-                      className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
-                    >
                       Delete all
                     </button>
                   </div>
